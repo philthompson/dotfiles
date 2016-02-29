5d61d39537ee7ec4d25abbebceca633c6628ec46
philthompson <philthompson22@gmail.com>
2016-02-29 11:59:20 -0800

    Fix install.sh to work with directories.
    Check if the destination link exists, rather than checking if it's a
    file.

--- install.sh
+++ install.sh
@@ -6,14 +6,15 @@ THIS_DIR="`dirname "${THIS_SCRIPT}"`"
 for DOTFILE in gitconfig bashrc bash_aliases vim vimrc Xresources
 do
   DEST="${HOME}/.${DOTFILE}"
-  if [ -f "${DEST}" ]
+  if [ -e "${DEST}" ]
   then
     if [ -L "${DEST}" ]
     then
-      rm "${DEST}"
+      rm -v "${DEST}"
     else
-      mv "${DEST}" "${HOME}/${DOTFILE}.saved.`date +%s`"
+      mv -v "${DEST}" "${HOME}/${DOTFILE}.saved.`date +%s`"
     fi
   fi
   ln -sv "${THIS_DIR}/${DOTFILE}" "${DEST}"
+  echo ""
 done
